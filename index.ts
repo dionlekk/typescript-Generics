f9b703c3869adde4219437d918813f5b6307dd53
dionlekk <74821838+dionlekk@users.noreply.github.com>
2023-04-16 21:07:56 +0300

    1 file modified

--- index.ts
+++ index.ts
@@ -1,15 +1,23 @@
-// Import stylesheets
-import './style.css';
+// // Import stylesheets
+// import './style.css';
 
-const names: Array<string> = ['Max', 'Manuel'];
+// const names: Array<string> = ['Max', 'Manuel'];
 
 
-const promise: Promise<string> = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve('This is done!');
-  },2000);
-});
+// const promise: Promise<string> = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve('This is done!');
+//   },2000);
+// });
 
-promise.then(data => {
-  data.split(' ');
-})
+// promise.then(data => {
+//   data.split(' ');
+// })
+
+
+function merge<T, U>(objA: T, objB: U): T & U {
+  return Object.assign(objA, objB);
+}
+
+const mergedObj = merge<{name: string, hobbies: string[]}, {age: number}>({name: 'Max', hobbies: ['Sports', 'Music']}, {age: 32});
+const mergedObj2 = merge({name: 'Max'}, {age: 32});
