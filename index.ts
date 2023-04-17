f273b087583436dfc2d01832eb238a8e9be97eb8
dionlekk <74821838+dionlekk@users.noreply.github.com>
2023-04-17 21:58:54 +0300

    1 file modified

--- index.ts
+++ index.ts
@@ -15,9 +15,74 @@
 // })
 
 
-function merge<T, U>(objA: T, objB: U): T & U {
+function merge<T extends object, U extends object>(objA: T, objB: U): T & U {
   return Object.assign(objA, objB);
 }
 
 const mergedObj = merge<{name: string, hobbies: string[]}, {age: number}>({name: 'Max', hobbies: ['Sports', 'Music']}, {age: 32});
+
 const mergedObj2 = merge({name: 'Max'}, {age: 32});
+
+// ******************************
+
+interface Lengthy {
+  length: number;
+}
+
+function countAndDescribe<T extends Lengthy>(element: T): [T, string]{
+  let descriptionText = 'Got no value.';
+  if (element.length === 1) {
+    descriptionText = 'Got one element.';
+  } else if (element.length > 1) {
+    descriptionText = 'Got ' + element.length + ' elements.';
+  }
+  return [element, descriptionText];
+}
+console.log(countAndDescribe('Hi there.'));
+
+
+
+// **********************************************
+
+
+
+function extractAndConvert<T extends object, U extends keyof T>(obj: T, key: U) {
+  return 'Value: ' + obj[key];
+}
+
+extractAndConvert({name: 'Dion'}, 'name');
+
+
+// ***********************************************
+
+
+class DataStorage<T> {
+  private data: T[] = [];
+
+  addItem(item: T) {
+    this.data.push(item);
+  }
+
+  removeItem(item: T) {
+    this.data.splice(this.data.indexOf(item), 1);
+  }
+
+  getItems() {
+    return [...this.data];
+  }
+
+  printData() {
+    console.log(this.data);
+  }
+}
+
+
+const textStorage = new DataStorage<string>();
+
+textStorage.addItem('Dion');
+textStorage.addItem('Dimitris');
+console.log(textStorage.getItems());
+textStorage.removeItem('Dimitris');
+console.log(textStorage.getItems());
+
+const numberStorage = new DataStorage<number>();
